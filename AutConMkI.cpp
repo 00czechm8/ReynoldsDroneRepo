b4fd143fb8c36327b33e465ca0e0b90f64931733
Mike Reynolds <mwreynol@umich.edu>
2024-07-14 12:28:58 -0400

    Honestly I forget

--- AutConMkI.cpp
+++ AutConMkI.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include "AutoControl.h"
+#include <chrono>
 
 int main(){
     SSModel testmodel;
-    float x[6] =  {1, 2, 3, 4, 5, 6};
-    testmodel.AssignX(x);
+
+    auto start_time = std::chrono::system_clock::now();
+    
+    float x[12] =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    float r[12] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120};
+    for(int i = 0; i<=0;i++){
+
+        testmodel.AssignX(x);
+        testmodel.AssignR(r);
+        testmodel.CalculateError();
+        float roll_com = testmodel.CalculateRoll();
+        float pitch_com = testmodel.CalculatePitch();
+        float Vx_com = testmodel.CalculateVx();
+        
+        }
+
+    auto end_time = std::chrono::system_clock::now();
+    std::chrono::duration<double> samp_time = end_time-start_time;
+    std::cout<<samp_time.count();
+   
     return 0;
 }
--- AutoControl.h
+++ AutoControl.h
@@ -1,31 +1,71 @@
 class SSModel {
     public:
-    
-    float A[6][6];
-    float B[6][5];
+    float samp_time = 0.5;
+
+    float A[12][12];
+    float B[12][3];
     float C[6][6];
     
-    
-    float r[6];
-    float x[6];
-    float e[6];
-    
-    //Create a linearized state-space model
-    void GenLSSM(){
+    //let the state vector be of the form [x, y, z, Vx, Vy, Vz, roll, pitch, yaw, roll_rate, pit_rate, yaw_rate]
+    float r[12];
+    float x[12];
+    float e[12];
+    float e_past[13] = {0, 0, 0};
+    float integral_error[3] = {0, 0, 0};
+    float PID_matr[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+    float command[3] = {0, 0, 0};
 
+    //
+    
+
+    //Create a linearized state-space model [unfinished]
+    void GenLSSM(){
+        std::cout<<"Don't use this yet, jackass";
     }
 
+    //Method to write reference into the class
+    void AssignR(float reference[12]){
+        for(int i = 0; i <= 11; i++){
+            r[i] = reference[i];
+        }
+    }
+    
     //Method to write state into the class
-    void AssignX(float state[6]){
-        for(int i = 0; i <= 6; i++){
+    void AssignX(float state[12]){
+        for(int i = 0; i <= 11; i++){
             x[i] = state[i];
         }
     }
     
     //Calculate error between the reference and the current state trjectory
-    void CalErr(){
-        for(int i = 0; i <= 6; i++){
+    void CalculateError(){
+        for(int i = 0; i <= 11; i++){
             e[i] = r[i]-x[i];
         }
     }
+    
+    //PID Control to calculate velocity
+    float CalculateVx(){
+        command[0] = PID_matr[0][0]*( (e[3]-e_past[0])/samp_time) + PID_matr[0][1]*e[3] + PID_matr[0][2]*(e[3]*samp_time+integral_error[0]);
+        e_past[0] = e[3];
+        integral_error[0] += e[3]*samp_time;
+        return command[0];
+    }
+
+    //PID control for roll
+    float CalculateRoll(){
+        command[1] = PID_matr[1][0]*( (e[6]-e_past[1])/samp_time) + PID_matr[1][1]*e[6] + PID_matr[1][2]*(e[6]*samp_time+integral_error[1]);
+        e_past[1] = e[6];
+        integral_error[1] += e[6]+samp_time;
+        return command[1];
+    }
+
+    //PID control for pitch
+    float CalculatePitch(){
+        command[2] = PID_matr[2][0]*( (e[7]-e_past[2])/samp_time) + PID_matr[2][1]*e[7] + PID_matr[2][2]*(e[7]*samp_time+integral_error[2]);
+        e_past[2] = e[7];
+        integral_error[2] += e[7]*samp_time;
+        return command[2];
+        }
+    
 };
